7ad98a3ee08df9d54d4b1045d755ce6a02b4571e
xiaobianchen <chenxiaobian89@163.com>
2015-09-10 13:24:59 +0800

    added DBUtils

--- src/main/java/com/dmc/crawler/WeiboCrawler.java
+++ src/main/java/com/dmc/crawler/WeiboCrawler.java
@@ -1,46 +1,46 @@
-package com.dmc.crawler;
-
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import cn.edu.hfut.dmic.webcollector.crawler.DeepCrawler;
-import cn.edu.hfut.dmic.webcollector.model.Links;
-import cn.edu.hfut.dmic.webcollector.model.Page;
-import cn.edu.hfut.dmic.webcollector.net.HttpRequesterImpl;
-
-
-/**
- * 
- * Created by Xiaobian Chen on 2015年8月5日
- *
- *
- */
-public class WeiboCrawler extends DeepCrawler {
-
-	public WeiboCrawler(String crawlPath) throws Exception {
-		super(crawlPath);
-		String cookie = WeiboCN.getSinaCookie("chenxiaobian89@163.com", "ch4890467");
-		HttpRequesterImpl requester = (HttpRequesterImpl) this.getHttpRequester();
-		requester.setCookie(cookie);
-	}
-
-	@Override
-	public Links visitAndGetNextLinks(Page page) {
-		//抽取微博
-		Elements weibos = page.getDoc().select("div.c");
-		for(Element weibo:weibos){
-			System.out.println(weibo.text());
-		}
-		return null;
-	}
-	
-	public static void main(String[] args) throws Exception {
-		WeiboCrawler crawler = new WeiboCrawler("crawler");
-        crawler.setThreads(3);  
-        /*对某人微博前5页进行爬取*/  
-        for(int i=0;i<5;i++){  
-            crawler.addSeed("http://weibo.cn/vipgcu?vt=4&page="+i);  
-        }  
-        crawler.start(1);  
-	}
-}
+//package com.dmc.crawler;
+//
+//import org.jsoup.nodes.Element;
+//import org.jsoup.select.Elements;
+//
+//import cn.edu.hfut.dmic.webcollector.crawler.DeepCrawler;
+//import cn.edu.hfut.dmic.webcollector.model.Links;
+//import cn.edu.hfut.dmic.webcollector.model.Page;
+//import cn.edu.hfut.dmic.webcollector.net.HttpRequesterImpl;
+//
+//
+///**
+// *
+// * Created by Xiaobian Chen on 2015年8月5日
+// *
+// *
+// */
+//public class WeiboCrawler extends DeepCrawler {
+//
+//	public WeiboCrawler(String crawlPath) throws Exception {
+//		super(crawlPath);
+//		String cookie = WeiboCN.getSinaCookie("chenxiaobian89@163.com", "ch4890467");
+//		HttpRequesterImpl requester = (HttpRequesterImpl) this.getHttpRequester();
+//		requester.setCookie(cookie);
+//	}
+//
+//	@Override
+//	public Links visitAndGetNextLinks(Page page) {
+//		//抽取微博
+//		Elements weibos = page.getDoc().select("div.c");
+//		for(Element weibo:weibos){
+//			System.out.println(weibo.text());
+//		}
+//		return null;
+//	}
+//
+//	public static void main(String[] args) throws Exception {
+//		WeiboCrawler crawler = new WeiboCrawler("crawler");
+//        crawler.setThreads(3);
+//        /*对某人微博前5页进行爬取*/
+//        for(int i=0;i<5;i++){
+//            crawler.addSeed("http://weibo.cn/vipgcu?vt=4&page="+i);
+//        }
+//        crawler.start(1);
+//	}
+//}
---
+++ src/main/java/com/dmc/utils/DBUtils.java
@@ -0,0 +1,48 @@
+package com.dmc.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ * Created by xiaobianchen on 15/9/9.
+ */
+public class DBUtils {
+
+    private static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+    private static final String URL = "jdbc:mysql://localhost:3306/dmc?autoReconnect=true";
+
+    /**
+     *
+     * @param url
+     * @param username
+     * @param password
+     * @return
+     */
+    public static Connection openConenection(String url,String username,String password){
+
+        Connection connection = null;
+        try {
+             Class.forName(DRIVER_NAME);
+             connection = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+
+    }
+
+    public static void main(String[] args) {
+        DBUtils.openConenection(URL,USERNAME,PASSWORD);
+    }
+
+
+
+
+
+
+}
