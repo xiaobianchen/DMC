0e3ca229446d8625108ddecc3b5fc33cf650f38d
xiaobianchen <chenxiaobian89@163.com>
2015-09-15 14:31:32 +0800

    modify files,applicationContext.xml mysql port change to 3307 for macbook, for windows still 3306

--- src/main/java/com/dmc/utils/DBUtils.java
+++ src/main/java/com/dmc/utils/DBUtils.java
@@ -3,6 +3,7 @@ package com.dmc.utils;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
 /**
  * Created by xiaobianchen on 15/9/9.
  */
@@ -11,21 +12,21 @@ public class DBUtils {
     private static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
-    private static final String URL = "jdbc:mysql://localhost:3306/dmc?autoReconnect=true";
+    private static final String URL = "jdbc:mysql://localhost:3307/dmc?autoReconnect=true";
 
     /**
-     *
      * @param url
      * @param username
      * @param password
      * @return
      */
-    public static Connection openConenection(String url,String username,String password){
+    public static Connection openConenection(String url, String username, String password) {
 
         Connection connection = null;
         try {
-             Class.forName(DRIVER_NAME);
-             connection = DriverManager.getConnection(url, username, password);
+            Class.forName(DRIVER_NAME);
+            connection = DriverManager.getConnection(url, username, password);
+            System.out.println("mysql connection successfully!");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
@@ -37,12 +38,8 @@ public class DBUtils {
     }
 
     public static void main(String[] args) {
-        DBUtils.openConenection(URL,USERNAME,PASSWORD);
+        DBUtils.openConenection(URL, USERNAME, PASSWORD);
     }
-
-
-
-
 
 
 }
--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -4,29 +4,104 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 CREATE DATABASE 'dmc' DEFAULT CHARACTER SET utf8 COLLATE utf8_bin; 
 USE 'dmc';
 
-
 /*Create Table User*/
-CREATE TABLE `user` (
-  `username` varchar(50) NOT NULL COMMENT '用户名',
-  `password` varchar(50) NOT NULL COMMENT '密码',
-  `email` varchar(50) NOT NULL COMMENT '邮箱',
-  `phone` varchar(20) NOT NULL COMMENT '联系电话',
-  `registerDate` datetime NOT NULL COMMENT '注册时间',
-  PRIMARY KEY (`username`,`email`,`phone`)
+CREATE TABLE 'user' (
+  'username' varchar(50) NOT NULL COMMENT '用户名',
+  'password' varchar(50) NOT NULL COMMENT '密码',
+  'email' varchar(50) NOT NULL COMMENT '邮箱',
+  'phone' varchar(20) NOT NULL COMMENT '联系电话',
+  'registerDate' datetime NOT NULL COMMENT '注册时间',
+  PRIMARY KEY ('username','email','phone')
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
 /*Create Table Comment*/
-CREATE TABLE `comment` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '评论编号',
-  `comments` varchar(255) DEFAULT NULL COMMENT '评论内容',
-  `type` varchar(50) DEFAULT NULL COMMENT '联系类型',
-  `contact` varchar(20) DEFAULT NULL COMMENT '联系方式',
-  `fileName` varchar(50) DEFAULT NULL COMMENT '存储图片',
-  `fileStream` longblob,
-  `reportTime` datetime DEFAULT NULL COMMENT '评论时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+CREATE TABLE 'comment' (
+  'id' int(11) NOT NULL,
+  'comments' varchar(255) NOT NULL,
+  'type' varchar(45) DEFAULT NULL,
+  'contact' varchar(45) DEFAULT NULL,
+  'reportTime' datetime DEFAULT NULL,
+  PRIMARY KEY ('comments','id')
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Create Table App*/
+CREATE TABLE 'app' (
+  '商家名称' varchar(255) DEFAULT NULL,
+  '日期' varchar(255) DEFAULT NULL,
+  '来源' varchar(255) DEFAULT NULL,
+  '来源明细' varchar(255) DEFAULT NULL,
+  '访客数' varchar(255) DEFAULT NULL,
+  '访客数变化' varchar(255) DEFAULT NULL,
+  '下单买家数' varchar(255) DEFAULT NULL,
+  '下单买家数变化' varchar(255) DEFAULT NULL,
+  '下单金额' varchar(255) DEFAULT NULL,
+  '下单金额变化' varchar(255) DEFAULT NULL,
+  '下单转化率' varchar(255) DEFAULT NULL,
+  '下单转化率变化' varchar(255) DEFAULT NULL,
+  '支付买家数' varchar(255) DEFAULT NULL,
+  '支付买家数变化' varchar(255) DEFAULT NULL,
+  '支付金额' varchar(255) DEFAULT NULL,
+  '支付金额变化' varchar(255) DEFAULT NULL,
+  '支付转化率' varchar(255) DEFAULT NULL,
+  '支付转化率变化' varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Create Table Flow*/
+CREATE TABLE 'flow' (
+  '商家名称' varchar(255) DEFAULT NULL,
+  '日期' varchar(255) DEFAULT NULL,
+  '来源' varchar(255) DEFAULT NULL,
+  '来源明细' varchar(255) DEFAULT NULL,
+  '访客数' varchar(255) DEFAULT NULL,
+  '访客数变化' varchar(255) DEFAULT NULL,
+  '下单买家数' varchar(255) DEFAULT NULL,
+  '下单买家数变化' varchar(255) DEFAULT NULL,
+  '下单金额' varchar(255) DEFAULT NULL,
+  '下单金额变化' varchar(255) DEFAULT NULL,
+  '下单转化率' varchar(255) DEFAULT NULL,
+  '下单转化率变化' varchar(255) DEFAULT NULL,
+  '支付买家数' varchar(255) DEFAULT NULL,
+  '支付买家数变化' varchar(255) DEFAULT NULL,
+  '支付金额' varchar(255) DEFAULT NULL,
+  '支付金额变化' varchar(255) DEFAULT NULL,
+  '支付转化率' varchar(255) DEFAULT NULL,
+  '支付转化率变化' varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Create Table PC*/
+CREATE TABLE 'pc' (
+  '商家名称' varchar(255) DEFAULT NULL,
+  '日期' varchar(255) DEFAULT NULL,
+  '来源' varchar(255) DEFAULT NULL,
+  '来源明细' varchar(255) DEFAULT NULL,
+  '访客数' varchar(255) DEFAULT NULL,
+  '访客数变化' varchar(255) DEFAULT NULL,
+  '新访客数' varchar(255) DEFAULT NULL,
+  '新访客数变化' varchar(255) DEFAULT NULL,
+  '浏览量' varchar(255) DEFAULT NULL,
+  '浏览量变化' varchar(255) DEFAULT NULL,
+  '人均浏览量' varchar(255) DEFAULT NULL,
+  '人均浏览量变化' varchar(255) DEFAULT NULL,
+  '收藏人数' varchar(255) DEFAULT NULL,
+  '收藏人数变化' varchar(255) DEFAULT NULL,
+  '加入购物车人数' varchar(255) DEFAULT NULL,
+  '加入购物车人数变化' varchar(255) DEFAULT NULL,
+  '跳失率' varchar(255) DEFAULT NULL,
+  '跳失率变化' varchar(255) DEFAULT NULL,
+  '下单金额' varchar(255) DEFAULT NULL,
+  '下单金额变化' varchar(255) DEFAULT NULL,
+  '下单买家数' varchar(255) DEFAULT NULL,
+  '下单买家数变化' varchar(255) DEFAULT NULL,
+  '下单转化率' varchar(255) DEFAULT NULL,
+  '下单转化率变化' varchar(255) DEFAULT NULL,
+  '支付金额' varchar(255) DEFAULT NULL,
+  '支付金额变化' varchar(255) DEFAULT NULL,
+  '支付买家数' varchar(255) DEFAULT NULL,
+  '支付买家数变化' varchar(255) DEFAULT NULL,
+  '支付转化率' varchar(255) DEFAULT NULL,
+  '支付转化率变化' varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 
 
