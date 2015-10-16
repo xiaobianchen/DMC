edbb6bb34db7c65e0bd6abe86b6f5fd795dc382c
xiaobianchen <chenxiaobian89@163.com>
2015-10-16 23:03:53 +0800

    updated db

--- src/main/java/com/dmc/controller/FlowController.java
+++ src/main/java/com/dmc/controller/FlowController.java
@@ -1,10 +1,8 @@
 package com.dmc.controller;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
