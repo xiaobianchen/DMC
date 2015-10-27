c7dc22daa2ebb2d5825ae93825215ef7e524569f
xiaobianchen <chenxiaobian89@163.com>
2015-10-27 12:39:07 +0800

    modify DBUtils.java

--- src/main/java/com/dmc/utils/DBUtils.java
+++ src/main/java/com/dmc/utils/DBUtils.java
@@ -1,27 +1,16 @@
 package com.dmc.utils;
 
 import java.sql.*;
-import java.util.Properties;
-
 /**
  * @author xiaobian.chen
  * @version 1.0 2015/9/10
  */
 public class DBUtils {
 
-    public static final Properties prop = new Properties();
-
-    static {
-        try {
-            prop.load(ClassLoader.getSystemResourceAsStream("jdbc.properties"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static final String USERNAME = prop.getProperty("username");
-    private static final String PASSWORD = prop.getProperty("password");
-    private static final String URL = prop.getProperty("url");
+    private static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+    private static final String URL = "jdbc:mysql://localhost:3306/dmc?autoReconnect=true";
     private static Connection connection = null;
     private static CallableStatement cstmt = null;
 
@@ -31,7 +20,7 @@ public class DBUtils {
      */
     public static Connection openConenection() {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName(DRIVER_NAME);
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
