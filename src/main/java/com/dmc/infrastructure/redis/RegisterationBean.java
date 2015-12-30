1f2e8957fcb34b66a076b710327002eeeffb333c
xiaobianchen <chenxiaobian89@163.com>
2015-12-30 11:12:11 +0800

    remove redis folder

--- src/main/java/com/dmc/infrastructure/redis/RegisterationBean.java
+++
@@ -1,9 +0,0 @@
-package com.dmc.infrastructure.redis;
-
-/**
- * @author xiaobianchen
- * @version 1.0 2015/12/29
- * @link https://github.com/xiaobianchen/DMC
- */
-public class RegisterationBean {
-}
--- src/main/java/com/dmc/utils/tt.java
+++
@@ -1,9 +0,0 @@
-package com.dmc.utils;
-
-/**
- * @author xiaobianchen
- * @version 1.0 2015/12/28
- * @link https://github.com/xiaobianchen/DMC
- */
-public class tt {
-}
