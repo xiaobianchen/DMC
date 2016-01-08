4ded5357d11b0f9cb19c760ad76f4e5b5f71b7a8
xiaobianchen <chenxiaobian89@163.com>
2016-01-08 11:05:42 +0800

    added memcached

---
+++ src/main/java/com/dmc/infrastructure/memcached/MemcachedTest.java
@@ -0,0 +1,21 @@
+package com.dmc.infrastructure.memcached;
+
+
+
+/**
+ * @author xiaobianchen
+ * @version 1.0 2016/1/7
+ * @link https://github.com/xiaobianchen/DMC
+ */
+public class MemcachedTest {
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+
+
+}
